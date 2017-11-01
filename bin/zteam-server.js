f2ff810b7ec153ba09771db7f016c5f089a0739a
Ramy Hakam <ramyhakam2@gmail.com>
2017-11-01 17:48:20 +0200

    first commit

---
+++ bin/zteam-server.js
@@ -0,0 +1,6 @@
+#!usr/bin/env node
+
+var chat_app = require("zteam-chat");
+chat_app.listen('3000', () => {
+    console.log(`server running on port 3000`);
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+var chat_app = require("zteam-chat");
+chat_app.listen('3000', () => {
+    console.log(`server running on port 3000`);
+})
